54b78372e71382fab7bc1d8294b7787934d6a6eb
Ariel Parra <arielp@eventbrite.com>
2018-09-05 14:40:14 -0300

    refactor code

--- eb_service.py
+++ eb_service.py
@@ -11,52 +11,67 @@ PLACE_ORDER_URL = "https://www.evbqaapi.com/v3/orders/{order_id}/place/?token={p
 
 
 def create_order(email, items):
-    create_order_data = {
-        "event_id": 49765664442,
-        "application": "manual",
+  create_order_response = _create_order(items)
+  update_order_response = _update_order(create_order_response['order']['id'], email)
+  add_payment_response = _add_payment_method(create_order_response['order']['id'])
+  place_order_response = _place_order(create_order_response['order']['id'])
+
+  return place_order_response
+
+
+def _create_order(items):
+  create_order_data = {
+      "event_id": 49765664442,
+      "application": "manual",
+  }
+  tickets = []
+  for item in items:
+      tickets.append({
+          "ticket_class_id": str(item['item_id']), "quantity": str(item['quantity'])
+      })
+  create_order_data['tickets'] = json.dumps(tickets)
+
+  create_order_request = requests.post(
+      CREATE_ORDER_URL, data=create_order_data)
+  return json.loads(create_order_request.text)
+
+
+def _update_order(order_id, email):
+  update_order_data = {
+    "order": {
+        "email": email
     }
-    tickets = []
-    for item in items:
-        tickets.append({
-            "ticket_class_id": str(item['item_id']), "quantity": str(item['quantity'])
-        })
-    create_order_data['tickets'] = json.dumps(tickets)
+  }
+  update_url = UPDATE_ORDER_URL.format(
+    personal_token=flask_app.config['EB_TOKEN'],
+    order_id=order_id,
+  )
+  update_order_request = requests.post(update_url, json=update_order_data)
+  return json.loads(update_order_request.text)
 
-    create_order_request = requests.post(CREATE_ORDER_URL, data=create_order_data)
-    create_order_response = json.loads(create_order_request.text)
 
-    update_order_data = {
-        "order": {
-            "email": email
-        }
-    }
-    update_url = UPDATE_ORDER_URL.format(
-      personal_token=flask_app.config['EB_TOKEN'],
-      order_id=create_order_response['order']['id'],
-    )
-    update_order_request = requests.post(update_url, json=update_order_data)
-    update_order_response = json.loads(update_order_request.text)
-
-    add_payment_data = {
-        "payment_methods": [
-            {
-                "payment_instrument_details": {
-                    "instrument_type": "MANUAL"
-                }
+def _add_payment_method(order_id):
+  add_payment_data = {
+    "payment_methods": [
+        {
+            "payment_instrument_details": {
+                "instrument_type": "MANUAL"
             }
-        ]
-    }
-    payment_url = ADD_PAYMENT_METHOD_URL.format(
-      personal_token=flask_app.config['EB_TOKEN'],
-      order_id=create_order_response['order']['id'],
-    )
-    payment_request = requests.post(payment_url, json=add_payment_data)
+        }
+    ]
+  }
+  payment_url = ADD_PAYMENT_METHOD_URL.format(
+    personal_token=flask_app.config['EB_TOKEN'],
+    order_id=order_id,
+  )
+  payment_request = requests.post(payment_url, json=add_payment_data)
+  return json.loads(payment_request.text)
 
-    place_url = PLACE_ORDER_URL.format(
-      personal_token=flask_app.config['EB_TOKEN'],
-      order_id=create_order_response['order']['id'],
-    )
-    place_order_request = requests.post(place_url)
-    place_order_response = json.loads(place_order_request.text)
 
-    return place_order_response
+def _place_order(order_id):
+  place_url = PLACE_ORDER_URL.format(
+    personal_token=flask_app.config['EB_TOKEN'],
+    order_id=order_id,
+  )
+  place_order_request = requests.post(place_url)
+  return json.loads(place_order_request.text)
